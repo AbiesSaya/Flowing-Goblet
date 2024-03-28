28c96d09cfd9eda740c693026a5b182471096ff0
Abies <1401419317@qq.com>
2024-03-28 21:37:28 +0800

    message

--- app/src/main/java/com/example/myapplication/UsageActivity.java
+++ app/src/main/java/com/example/myapplication/UsageActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class UsageActivity extends AppCompatActivity {
 //这里是最重要的一个界面，也就是我们再使用过程中寻找联系人的界面
+    //声明
+    private ImageView mPro;
+    private com.example.eva.SlideMenu slideMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,19 @@ public class UsageActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //find
+        mPro=findViewById(R.id.profile);
+        slideMenu=findViewById(R.id.SlideMenu);
+
+
+        //slide part
+        mPro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slideMenu.switchMenu();
+            }
         });
     }
 }
