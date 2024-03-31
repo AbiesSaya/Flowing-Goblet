80ff4562f236f4a1870c7f0cce1eefea6d1d0372
Abies <1401419317@qq.com>
2024-03-31 22:51:29 +0800

    newicon

--- app/src/main/java/com/example/myapplication/UsageActivity.java
+++ app/src/main/java/com/example/myapplication/UsageActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,16 +13,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UsageActivity extends AppCompatActivity {
-//这里是最重要的一个界面，也就是我们再使用过程中寻找联系人的界面
+//这里是最重要的一个界面，也就是我们再使用过程中寻找联系人的界面(其实不是）
     //声明
     private ImageView mPro;
     private com.example.eva.SlideMenu slideMenu;
+    private Button mBback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_usage);
+        //
+        mBback=findViewById(R.id.chat);
+
+        mBback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 =new Intent(UsageActivity.this, UsageActivity.class);
+
+                startActivity(intent1);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
